76c3a5d92867be40e3bfee5a0a9c36d381da4ad5
ricardober93 <ricardobzorro@gmail.com>
2020-09-06 15:44:09 -0500

    nabvar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Header from 'components/Header'
-import Navbar from 'components/navbar'
+import Navbar from 'components/Navbar'
 import  Feed from 'components/Feed'
 import { colors } from 'style/theme'
 export default function Home() {
